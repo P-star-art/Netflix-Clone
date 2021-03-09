bd8ed698a59aa295ed75819886bb9ff8c98de231
Pranav <prn2108@gmail.com>
2021-03-09 22:08:52 +0530

    feat: added navbar

---
+++ src/Containers/HomeScreen/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar';
+import './HomeScreen.css'
+
+function HomeScreen() {
+    return (
+        <div className="homeScreen">
+            <Navbar />
+            {/* <Banner />
+            <Row /> */}
+        </div>
+    )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCBzhhUoZdAwHlmFczgf-2d9rmmNpTbz8k",
+    authDomain: "netflix-clone-c6c52.firebaseapp.com",
+    projectId: "netflix-clone-c6c52",
+    storageBucket: "netflix-clone-c6c52.appspot.com",
+    messagingSenderId: "438611298761",
+    appId: "1:438611298761:web:6b2cfda02c302184504018",
+    measurementId: "G-T900H5WFCZ"
+  };
