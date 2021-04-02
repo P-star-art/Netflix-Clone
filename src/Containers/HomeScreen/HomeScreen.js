795f07c800d2d251766291c1450f4c37dd4d374a
Pranav <prn2108@gmail.com>
2021-04-03 00:42:30 +0530

    feat: added banner on homepage

--- src/Containers/HomeScreen/HomeScreen.js
+++ src/Containers/HomeScreen/HomeScreen.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar';
+import Banner from '../../Components/Banner/Banner';
 import './HomeScreen.css'
 
 function HomeScreen() {
     return (
         <div className="homeScreen">
             <Navbar />
-            {/* <Banner />
-            <Row /> */}
+             <Banner />
+           {/* <Row /> */}
         </div>
     )
 }
