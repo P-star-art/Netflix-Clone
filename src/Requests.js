a3880460ba259d0a39d3163a2b333528e988a0d3
Pranav <prn2108@gmail.com>
2021-04-03 23:58:00 +0530

    feat: implementing TMDB api for fetching movies for banner as well as other content

---
+++ src/Requests.js
@@ -0,0 +1,12 @@
+const API_KEY = "7ae2c2510fadb1c49b46262339c489f3";
+
+const requests = {
+    fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
