12521751e627e008d3225f8c86133bdaafab0128
Pranav <prn2108@gmail.com>
2021-04-08 01:13:27 +0530

    feat: adding firebase hosting and opening page of netflix with routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './Containers/HomeScreen/HomeScreen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Login from './Containers/Login/Login';
 
 function App() {
+
+  const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {
+          !user ? (
+            <Login />
+          ) : (
+            <Switch>
+              <Route exact path="/">
+                <HomeScreen />
+              </Route>
+            </Switch>
+          )
+        }
+      </Router>
     </div>
   );
 }
